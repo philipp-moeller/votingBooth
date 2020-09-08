748a9019856c18c1b7f320f111bf8ca3cfb1e710
Philipp Möller <philipp@kinscher-moeller.de>
2020-09-08 14:03:30 +0200

    authorize administrator by session cookie

--- vote.rb
+++ vote.rb
@@ -3,8 +3,14 @@ require 'sinatra'
 # use local-ip here
 set :bind, '0.0.0.0'
 set :port, 4444
+enable :sessions
 
-admin = 'vaOPhSbcDuvY5y8tr2hKGBn9dT5tdI5r'
+configFile = File.read('.config').split("\n")
+config = {}
+for row in configFile do
+	configData = row.split(':')
+	config[configData[0]] = configData[1]
+end
 
 def form(title,form)
 	return File.read('form.html').gsub('{{title}}',title).sub('{{form}}',form)
@@ -28,38 +34,79 @@ post '/' do
 	if valid?(code) then
 		redirect '/'+code
 	else
+		if code==config['adminPassword'] then
+			session[:admin] = true
+			redirect '/admin'
+		else
+			redirect '/'
+		end
+	end
+end
+
+get '/admin' do
+	if session[:admin] then
+		text = '<input type="button" onclick="window.location.href=\'/codes\'" value="Codes anzeigen">'
+		text += '<input type="button" onclick="window.location.href=\'/result\'" value="Ergebnis anzeigen">'
+		text += '<form method="post" action=""><input type="submit" value="abmelden" /></form>'
+		return plain('Administration',text)
+	else
 		redirect '/'
 	end
 end
 
-get '/result/'+admin do
-  options = File.read('options.txt').split("\n")
-  votes = File.read('votes.txt').split("\n")
-	results = {}
-	for option in options do
-		results[option] = 0
-	end
-	for submission in votes do
-		items = submission.split(':')[1].split(';')
-		for item in items do
-			results[item] += 1
-		end
-	end
-	content = votes.length.to_s+' mal abgestimmt<br /><br />'
-	for option in options do
-		content += option+' ('+results[option].to_s+')<br />'+'▮'*results[option]+'<br /><br />'
-	end
-  return plain('Ergebnisse',content)
+post '/admin' do
+	session[:admin] = false
+	redirect '/'
 end
 
-get '/codes/'+admin do
-	set = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	codes = []
-	for i in 0..30 do
-		codes.push(set[rand(set.length)]+set[rand(set.length)]+set[rand(set.length)])
+get '/result' do
+	if session[:admin] then
+	  options = File.read('options.txt').split("\n")
+	  votes = File.read('votes.txt').split("\n")
+		results = {}
+		for option in options do
+			results[option] = 0
+		end
+		for submission in votes do
+			items = submission.split(':')[1].split(';')
+			for item in items do
+				if results[item]!=nil then
+					results[item] += 1
+				end
+			end
+		end
+		content = votes.length.to_s+' mal abgestimmt<br /><br />'
+		for option in options do
+			content += option+' ('+results[option].to_s+')<br />'+'▮'*results[option]+'<br /><br />'
+		end
+	  return plain('Ergebnisse',content)
+	else
+		redirect '/'
 	end
-	File.write('codes.txt',codes.join("\n"))
-  return plain('Codes',codes.join('<br />'))
+end
+
+get '/codes' do
+	if session[:admin] then
+		codes = File.read('codes.txt').split("\n")
+		form = '<form method="post" action=""><input type="submit" value="Neu generieren" /></form>'
+	  return plain('Codes',codes.join('<br />')+form)
+	else
+		redirect '/'
+	end
+end
+
+post '/codes' do
+	if session[:admin] then
+		set = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+		codes = []
+		for i in 0..30 do
+			codes.push(set[rand(set.length)]+set[rand(set.length)]+set[rand(set.length)])
+		end
+		File.write('codes.txt',codes.join("\n"))
+	  redirect '/codes'
+	else
+		redirect '/'
+	end
 end
 
 get '/:code' do

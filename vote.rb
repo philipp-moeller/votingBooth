89df79fcfbb358ebb148d9c03b786607b3069bbc
Philipp Möller <philipp@kinscher-moeller.de>
2020-09-09 01:14:19 +0200

    style code overview in prspective of adding printing option

--- vote.rb
+++ vote.rb
@@ -12,8 +12,8 @@ for row in configFile do
 	config[configData[0]] = configData[1]
 end
 
-def form(title,form)
-	return File.read('form.html').gsub('{{title}}',title).sub('{{form}}',form)
+def form(title,form,text)
+	return File.read('form.html').gsub('{{title}}',title).sub('{{text}}',text).sub('{{form}}',form)
 end
 
 def plain(title,text)
@@ -26,7 +26,7 @@ end
 
 get '/' do
   form = '<input type="text" name="code" value="" /><br /><input type="submit" value="senden" />'
-  return form('Anmelden',form)
+  return form('Anmelden',form,'')
 end
 
 post '/' do
@@ -75,7 +75,8 @@ get '/result' do
 				end
 			end
 		end
-		content = votes.length.to_s+' mal abgestimmt<br /><br />'
+		content = '<a class="back" href="/admin">&#11176;</a>';
+		content += votes.length.to_s+' mal abgestimmt<br /><br />'
 		for option in options do
 			content += option+' ('+results[option].to_s+')<br />'+'▮'*results[option]+'<br /><br />'
 		end
@@ -88,8 +89,12 @@ end
 get '/codes' do
 	if session[:admin] then
 		codes = File.read('codes.txt').split("\n")
-		form = '<form method="post" action=""><input type="submit" value="Neu generieren" /></form>'
-	  return plain('Codes',codes.join('<br />')+form)
+		text = '<a class="back" href="/admin">&#11176;</a>';
+		for code in codes do
+			text += '<div class="card">'+code+'</div>'
+		end
+		text += '<form method="post" action=""><input type="submit" value="Neu generieren" /></form>'
+	  return plain('Codes',text)
 	else
 		redirect '/'
 	end
@@ -120,7 +125,7 @@ get '/:code' do
 		form += '<input type="button" value="'+option+'" onclick="clickButton(this);" /><br />'
 	end
 	form += '<input type="submit" value="senden" />'
-  return form('Abstimmen',form)
+  return form('Abstimmung',form,'')
 end
 
 post '/:code' do
@@ -135,13 +140,13 @@ post '/:code' do
 		end
 	end
 	vote = params['voted'].split(';')
+	text = '<a class="back" href="">&#11176;</a>';
 	if (vote.length==2) then
 		votes.push(code+':'+vote.join(';'))
 		File.write('votes.txt',votes.join("\n"))
-		text = '<p>Ihre Wahl wurde erfolgreich gespeichert.</p>'
+		text += '<p>Ihre Wahl wurde erfolgreich gespeichert.</p>'
 	else
-		text = '<p>Wählen Sie bitte genau zwei Projekte aus. Ihre Wahl wurde nicht gespeichert!</p>'
+		text += '<p>Wählen Sie bitte genau zwei Projekte aus. Ihre Wahl wurde nicht gespeichert!</p>'
 	end
-  text += '<p><a href="">zurück</a></p>'
   return plain('Abstimmung',text);
 end
